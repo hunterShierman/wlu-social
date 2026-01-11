4e3659c64d1c97e198e29f99c1c3953eea1c786e
Hunter Shierman <hshierman@gmail.com>
2026-01-11 18:13:38 -0500

    Feature: add cloudinary API and set up config file - download cloudinary SDK - create config file and add API keys to the .env file (not in this commit protected by .gitignore) - cloudinary will be used to store our photos and we will get the links from cloudinary instead of storing raw image blobs in postgres

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -6,5 +6,8 @@ export const config = {
   DATABASE_URL: process.env.DATABASE_URL || '',
   NODE_ENV: process.env.NODE_ENV || 'development',
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || '',
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || ''
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || '',
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || '',
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '',
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || ''
 };
